bcd6617801ce8589704170b49d1bd3024b817b22
akia-web <59091758+akia-web@users.noreply.github.com>
2021-03-07 02:35:34 +0100

    .

--- livres.js
+++ livres.js
@@ -27,98 +27,98 @@ $(function() {
         }
     })
 
-  
+    let audio = new Audio('page.wav');
+
+    // animation des livres
+    $("#html" ).on({
+        
+        "mouseover":function(){
+           this.src = 'images/livre-html-ouvert.svg';
+           audio.play();
+         
+        },
+        "mouseout": function(){
+           
+           this.src="images/livre-html.svg";
+           
+        }
+     })
+    
+     $("#css" ).on({
+        "mouseover":function(){
+           this.src = 'images/livre-css-ouvert.svg';
+           audio.play();
+         
+        },
+        "mouseout": function(){
+           
+           this.src="images/livre-css.svg";
+          
+           
+        }
+     })
+    
+    
+     $("#js" ).on({
+        "mouseover":function(){
+           this.src = 'images/livre-js-ouvert.svg';
+           audio.play();
+         
+        },
+        "mouseout": function(){
+           
+           this.src="images/livre-js.svg";
+           
+        }
+     })
+    
+    
+    // affiche le contenu du livre HTML
+    const onglets = document.querySelectorAll('.onglets');
+    const contenu = document.querySelectorAll('.contenu');
+    
+    let index = 0;
+    
+    onglets.forEach(onglet => {
+    
+        onglet.addEventListener('click', () => {
+    
+            if(onglet.classList.contains('active')){
+                return;
+            } else {
+                onglet.classList.add('active');
+            }
+    
+            index = onglet.getAttribute('data-anim');
+            console.log(index);
+            
+            for(i = 0; i < onglets.length; i++) {
+    
+                if(onglets[i].getAttribute('data-anim') != index) {
+                    onglets[i].classList.remove('active');
+                }
+    
+            }
+    
+            for(j = 0; j < contenu.length; j++){
+    
+                if(contenu[j].getAttribute('data-anim') == index) {
+                    contenu[j].classList.add('activeContenu');
+                } else {
+                    contenu[j].classList.remove('activeContenu');
+                }
+                
+    
+            }
+    
+    
+        })
+    
+    })
 
 
 });
 
-let audio = new Audio('page.wav');
 
-// animation des livres
-$("#html" ).on({
-    
-    "mouseover":function(){
-       this.src = 'images/livre-html-ouvert.svg';
-       audio.play();
-     
-    },
-    "mouseout": function(){
-       
-       this.src="images/livre-html.svg";
-       
-    }
- })
-
- $("#css" ).on({
-    "mouseover":function(){
-       this.src = 'images/livre-css-ouvert.svg';
-       audio.play();
-     
-    },
-    "mouseout": function(){
-       
-       this.src="images/livre-css.svg";
-      
-       
-    }
- })
-
-
- $("#js" ).on({
-    "mouseover":function(){
-       this.src = 'images/livre-js-ouvert.svg';
-       audio.play();
-     
-    },
-    "mouseout": function(){
-       
-       this.src="images/livre-js.svg";
-       
-    }
- })
-
-
-// affiche le contenu du livre HTML
-const onglets = document.querySelectorAll('.onglets');
-const contenu = document.querySelectorAll('.contenu');
-
-let index = 0;
-
-onglets.forEach(onglet => {
-
-    onglet.addEventListener('click', () => {
-
-        if(onglet.classList.contains('active')){
-            return;
-        } else {
-            onglet.classList.add('active');
-        }
-
-        index = onglet.getAttribute('data-anim');
-        console.log(index);
-        
-        for(i = 0; i < onglets.length; i++) {
-
-            if(onglets[i].getAttribute('data-anim') != index) {
-                onglets[i].classList.remove('active');
-            }
-
-        }
-
-        for(j = 0; j < contenu.length; j++){
-
-            if(contenu[j].getAttribute('data-anim') == index) {
-                contenu[j].classList.add('activeContenu');
-            } else {
-                contenu[j].classList.remove('activeContenu');
-            }
-            
-
-        }
-
-
-    })
-
-})
 
 
